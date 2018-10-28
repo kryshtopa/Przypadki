41c1acc2158cae5c8a9b41dfe4acc7ba7db42ffd
Dmitry Kryshtopa <dmitry.kryshtopa@gmail.com>
2018-10-28 23:23:25 +0200

    app v 0.1

---
+++ precache-manifest.56b5f576da7fa1335fd24e862fa83720.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d250deeaf60b5ecebfd83093b7df5a0b",
+    "url": "/przypadki/static/media/just-waves.d250deea.png"
+  },
+  {
+    "revision": "2accfce096eb74e3485d",
+    "url": "/przypadki/static/js/runtime~main.2accfce0.js"
+  },
+  {
+    "revision": "1b74bf4976ab80e37e77",
+    "url": "/przypadki/static/js/main.1b74bf49.chunk.js"
+  },
+  {
+    "revision": "3d98690968a512237e46",
+    "url": "/przypadki/static/js/1.3d986909.chunk.js"
+  },
+  {
+    "revision": "1b74bf4976ab80e37e77",
+    "url": "/przypadki/static/css/main.367d93e8.chunk.css"
+  },
+  {
+    "revision": "3d98690968a512237e46",
+    "url": "/przypadki/static/css/1.445a9b85.chunk.css"
+  },
+  {
+    "revision": "2046c78054982b8f902619475aa9573f",
+    "url": "/przypadki/index.html"
+  }
+];
